3f789d504bb9d5efc68ed6667fb4a6e36be6a56d
Randy Sofia <randysofia@gmail.com>
2017-03-31 15:50:05 -0400

    If saving in mongo, use mongo prescribed geo format and objectids

--- mongo.go
+++ mongo.go
@@ -5,9 +5,33 @@ import (
 	"os"
 
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
-func (e *Event) mongosave() {
+type geo struct {
+	Lng float32 `bson:"lng,omitempty"`
+	Lat float32 `bson:"lat,omitempty"`
+}
+
+type mongoe struct {
+	Event       `bson:",inline"`
+	MongoUserID bson.ObjectId `bson:"userid_mgo,omitempty"`
+	GeoCoord    geo           `bson:"geo_coord,omitempty"`
+}
+
+func (standarde *Event) mongosave() {
+
+	var e mongoe
+
+	if !bson.IsObjectIdHex(standarde.UserID) {
+		e = mongoe{*standarde, bson.ObjectId(""), geo{0, 0}}
+	} else {
+		e = mongoe{*standarde, bson.ObjectIdHex(standarde.UserID), geo{0, 0}}
+	}
+	e.GeoCoord.Lat = standarde.GeoLatitude
+	e.GeoCoord.Lng = standarde.GeoLongitude
+	e.GeoLatitude = 0
+	e.GeoLongitude = 0
 	session, err := mgo.Dial(os.Getenv("MONGO_URI"))
 
 	if err != nil {
