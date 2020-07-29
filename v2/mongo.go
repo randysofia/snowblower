0c49d78274eb446685151c97b53577b6388e7ae4
Balint Antal <balint@riltech.co>
2020-07-29 05:08:00 +0200

    upgrade: make geocoord optional

--- v2/mongo.go
+++ v2/mongo.go
@@ -4,18 +4,14 @@ import (
 	"context"
 	"os"
 
+	"github.com/spark451/snowman"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
-
-type geo struct {
-	Lng float32 `bson:"lng,omitempty"`
-	Lat float32 `bson:"lat,omitempty"`
-}
 
 type mongoe struct {
 	Event       `bson:",inline"`
 	MongoUserID primitive.ObjectID `bson:"userid_mgo,omitempty"`
-	GeoCoord    geo                `bson:"geo_coord,omitempty"`
+	GeoCoord    *snowman.Geo       `bson:"geo_coord,omitempty"`
 }
 
 func (standarde *Event) mongosave() {
@@ -23,14 +19,18 @@ func (standarde *Event) mongosave() {
 	var e mongoe
 
 	if oid, err := primitive.ObjectIDFromHex(standarde.UserID); err == nil {
-		e = mongoe{*standarde, oid, geo{0, 0}}
+		e = mongoe{*standarde, oid, nil}
 	} else {
-		e = mongoe{*standarde, primitive.NewObjectID(), geo{0, 0}}
+		e = mongoe{*standarde, primitive.NewObjectID(), nil}
 	}
-	e.GeoCoord.Lat = standarde.GeoLatitude
-	e.GeoCoord.Lng = standarde.GeoLongitude
-	e.GeoLatitude = 0
-	e.GeoLongitude = 0
+	if e.GeoLatitude != 0 || e.GeoLongitude != 0 {
+		e.GeoCoord = &snowman.Geo{
+			Lat: standarde.GeoLatitude,
+			Lng: standarde.GeoLongitude,
+		}
+		e.GeoLatitude = 0
+		e.GeoLongitude = 0
+	}
 
 	if _, err := client.
 		Database(os.Getenv("MONGO_DB")).
